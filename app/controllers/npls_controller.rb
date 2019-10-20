240590d1a5832ce50bb75de0f7af444b153e3481
Alan Mareines <alanmareines@gmail.com>
2019-10-20 14:19:53 -0300

    add methods new and create for npl

--- app/controllers/npls_controller.rb
+++ app/controllers/npls_controller.rb
@@ -1,9 +1,37 @@
 class NplsController < ApplicationController
+  before_action :find_npl, only: %i[edit show]
+
   def index
     @npls = Npl.all
   end
 
+  def new
+    @npl = Npl.new
+  end
+
+  def create
+    @user = current_user
+    @npl = Npl.new(npl_params)
+    @npl.user = @user
+    if @npl.save
+      redirect_to npl_path(@npl)
+    else
+      render :new
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def find_npl
     @npl = Npl.find(params[:id])
+  end
+
+  def npl_params
+    params.require(:npl).permit(:name, :book_value, :min_value,
+                                :collateral_description, :debtor,
+                                :maturity_date, :npl_type, :user_id)
   end
 end
