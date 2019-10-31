d2cf6459b4e36b1cf99200f470623e1d856b32a6
Alan Mareines <alanmareines@gmail.com>
2019-10-31 18:33:45 -0300

    route for cockpit and start of front-end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def cockpit
+    @user = current_user
+    @bids = @user.bids
+    @npls = Npl.where(user_id: @user)
+  end
 end
