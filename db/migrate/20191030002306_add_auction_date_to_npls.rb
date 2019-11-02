5478ee0e76562fa6cfc332ef376ae2c3bf45db5a
Felipe Rocha <felipegirocha@gmail.com>
2019-11-02 15:20:21 -0300

    without migrations

--- db/migrate/20191030002306_add_auction_date_to_npls.rb
+++
@@ -1,5 +0,0 @@
-class AddAuctionDateToNpls < ActiveRecord::Migration[5.2]
-  def change
-    add_column :npls, :auction_date, :date
-  end
-end
--- db/migrate/20191030002332_add_auctioned_to_npls.rb
+++
@@ -1,5 +0,0 @@
-class AddAuctionedToNpls < ActiveRecord::Migration[5.2]
-  def change
-    add_column :npls, :auctioned, :boolean, default: false
-  end
-end
