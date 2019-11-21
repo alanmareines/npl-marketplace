9d76de05bc99c23acbabbb54178bc4df4ce40c49
Alan Mareines <alanmareines@gmail.com>
2019-11-21 20:19:42 -0300

    minhas bolas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/users/:id', to: 'pages#cockpit', as: 'user_cockpit'
+  get '/cockpit', to: 'pages#cockpit', as: 'user_cockpit'
   resources :npls do
     resources :bids, only: [:index, :new, :create]
     post "run_auction", to: "npls#run_auction"
