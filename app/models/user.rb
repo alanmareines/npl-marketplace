b6eb25ef8f46a2fb57aa0adac23f7bd584233644
Marco Jesus <macpjesus@gmail.com>
2019-11-23 15:55:08 -0300

    desabled mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,9 @@ class User < ApplicationRecord
   validates :name, :cpf_cnpj, :phone, presence: true
 
   # send an welcome email after sucessful sign-up
+  #########
   # after_create :send_welcome_email
-
+  #########
   private
 
   def send_welcome_email
