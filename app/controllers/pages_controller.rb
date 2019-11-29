c40a0be75f2b94d442c361cc07ffbbfc1be26aa3
Felipe Rocha <felipegirocha@gmail.com>
2019-11-29 18:06:09 -0300

    highlights working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,17 @@ class PagesController < ApplicationController
   def cockpit
     @user = current_user
     @bids = @user.bids
-    @npls = Npl.where(user_id: @user)
+    @npls = @user_npls_book_value
+    @bids_winner_sum = 0
+    user_bids_npls = Npl.where(user: current_user, auctioned: true)
+    user_bids_npls.each do |npl|
+      @bids_winner_sum += npl.bids.order(value: :desc).first.value
+    end
+
   end
 
   def errorpage
   end
+
+
 end
