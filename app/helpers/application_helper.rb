76087475fe2717758c3139ad3717411f24100ae3
Marco Jesus <macpjesus@gmail.com>
2019-11-28 22:25:00 -0300

    4 steps user jorney

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def api_call
+    require 'json'
+    require 'open-uri'
+
+    url = 'https://api.github.com/users/ssaunier'
+    user_serialized = open(url).read
+    user = JSON.parse(user_serialized)
+  end
+
 end
